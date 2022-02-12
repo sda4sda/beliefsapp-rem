135b80de73600b2e775bb790b4b7f2ee57550cc3
Daniel Scholtus <sdeonline@gmail.com>
2022-02-12 18:05:41 -0300

    Update source component path

---
+++ src/main/java/org/sda4sda/beliefsapp/rem/MySpringBootApplication.java
@@ -0,0 +1,14 @@
+package org.sda4sda.beliefsapp.rem;
+
+import org.sda4sda.beliefsapp.rem.MySpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sda4sda/beliefsapp/rem/MySpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.sda4sda.beliefsapp.rem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
